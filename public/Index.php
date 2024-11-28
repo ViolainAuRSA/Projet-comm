47b899b65316956fd996c8400ea873c978b34ec9
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-11-28 12:56:28 +0100

    Ajout des liens vers les pages

--- public/Index.php
+++ public/Index.php
@@ -13,6 +13,9 @@ require_once __DIR__ . '/../Database1.php';
 <body>
     <header>
         <img src="logo_site.jpg"alt="logo du site">
+        <a href="#">Nouveautés</a>
+        <a href="#">Produits</a>
+        <a href="#">En savoir plus </a>
     </header>
     <h1>Le sablier tranquille</h>
 </body>

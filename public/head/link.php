024b4758fac5941db738472726a14f1cc20a31e5
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-12-11 10:42:28 +0100

    remise en ordre du css

--- public/head/link.php
+++ public/head/link.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
--- public/menu/menu.php
+++ public/menu/menu.php
@@ -22,6 +22,9 @@
                         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                     </svg>
                 </button>
+                <?php 
+                if(!isset($_SESSION['id'])){
+                ?>
                 <a href="register.php" class="login-btn">
                     <span class="login-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
@@ -30,6 +33,18 @@
                         </svg>
                     </span>
                 </a>
+                <?php }else {
+                ?>
+                 <a href="deconnexion.php" class="login-btn">
+                    <span class="login-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                            <circle cx="12" cy="7" r="4"></circle>
+                            <path d="M5.5 21h13c1 0 2-1 2-2.5 0-3.5-4-6.5-8-6.5s-8 3-8 6.5c0 1.5 1 2.5 2 2.5z"></path>
+                        </svg>
+                    </span>
+                </a>
+
+                <?php } ?>
                     <!-- Boutons séparés -->
                 <a href="cart.php" class="btn-cart">
                     <i class="fas fa-shopping-cart"></i>

f34b2e615a2de21ab42a6221af16e5f16bd40c8a
ViolainAuRSA <samuel.bergerbidaud@gmail.com>
2024-12-18 01:02:08 +0100

    ajout de la fonction ajout au panier pour tous les articles

--- public/product.php
+++ public/product.php
@@ -24,7 +24,6 @@
     }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -40,8 +39,6 @@
     <section class="featured-products">
         <h1>Nos produits pour votre bien-être</h1>
         <div class="product-container">
-
-            <div class="product-container">
 
 
 
@@ -91,12 +88,16 @@
                     <a href="login.php"><button>Ajouter au panier</button></a>
                 <?php } else { ?>
                     <form method="post" action="">
+                        <input type="hidden" name="product_id" value="8">
                         <input type="hidden" name="product_name" value="Bougie de Sérénité">
                         <input type="hidden" name="product_price" value="19.99">
+                        <input type="hidden" name="product_quantity" value="1">
                         <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
                     </form>
                 <?php } ?>
             </div>
+
+
             <div class="product-card">
                 <img src="img/oreiller-meditation-ergonomique.jpg" alt="Oreiller de Méditation Ergonomique">
                 <h2>Oreiller de Méditation Ergonomique</h2>
@@ -105,42 +106,72 @@
                     <a href="login.php"><button>Ajouter au panier</button></a>
                 <?php } else { ?>
                     <form method="post" action="">
-                        <input type="hidden" name="product_name" value="Encens Relaxants">
-                        <input type="hidden" name="product_price" value="9.99">
+                        <input type="hidden" name="product_id" value="9">
+                        <input type="hidden" name="product_name" value="Oreiller de Méditation Ergonomique">
+                        <input type="hidden" name="product_price" value="29.99">
+                        <input type="hidden" name="product_quantity" value="1">
                         <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
                     </form>
                 <?php } ?>
             </div>
+
+
         <div class="product-card">
             <img src="img/Huiles-Essentielles-Relaxantes.jpg" alt="Huiles Essentielles Relaxantes">
             <h2>Huiles Essentielles Relaxantes</h2>
             <p>Prix : 14,99€</p>
-            <?php if(!isset($_SESSION['id'])){ ?>
-                <a href="login.php"><button>Ajouter au panier</button></a>
-            <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
-            <?php } ?>
-        </div>
+            <?php if (!isset($_SESSION['id'])) { ?>
+                    <a href="login.php"><button>Ajouter au panier</button></a>
+                <?php } else { ?>
+                    <form method="post" action="">
+                        <input type="hidden" name="product_id" value="10">
+                        <input type="hidden" name="product_name" value="Huiles Essentielles Relaxantes">
+                        <input type="hidden" name="product_price" value="14.99">
+                        <input type="hidden" name="product_quantity" value="1">
+                        <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                    </form>
+                <?php } ?>
+            </div>
+
+
+
         <div class="product-card">
             <img src="img/Bol-Chantant-Tibetain.jpg" alt="Bol Chantant Tibétain">
             <h2>Bol Chantant Tibétain</h2>
             <p>Prix : 49,99€</p>
-            <?php if(!isset($_SESSION['id'])){ ?>
-                <a href="login.php"><button>Ajouter au panier</button></a>
-            <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
-            <?php } ?>
-        </div>
+            <?php if (!isset($_SESSION['id'])) { ?>
+                    <a href="login.php"><button>Ajouter au panier</button></a>
+                <?php } else { ?>
+                    <form method="post" action="">
+                        <input type="hidden" name="product_id" value="11">
+                        <input type="hidden" name="product_name" value="Bol Chantant Tibétain">
+                        <input type="hidden" name="product_price" value="49.99">
+                        <input type="hidden" name="product_quantity" value="1">
+                        <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                    </form>
+                <?php } ?>
+            </div>
+
+
+
         <div class="product-card">
             <img src="img/Diffuseur-Huiles-Essentielles.jpg" alt="Diffuseur d'Huiles Essentielles">
             <h2>Diffuseur d'Huiles Essentielles</h2>
             <p>Prix : 39,99€</p>
-            <?php if(!isset($_SESSION['id'])){ ?>
-                <a href="login.php"><button>Ajouter au panier</button></a>
-            <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
-            <?php } ?>
-        </div>
+            <?php if (!isset($_SESSION['id'])) { ?>
+                    <a href="login.php"><button>Ajouter au panier</button></a>
+                <?php } else { ?>
+                    <form method="post" action="">
+                        <input type="hidden" name="product_id" value="12">
+                        <input type="hidden" name="product_name" value="Diffuseur d'Huiles Essentielles">
+                        <input type="hidden" name="product_price" value="39.99">
+                        <input type="hidden" name="product_quantity" value="1">
+                        <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                    </form>
+                <?php } ?>
+            </div>
+
+
         <div class="product-card">
             <img src="img/Chaleur-Douce-Coussin-Chauffant.jpeg" alt="Chaleur Douce - Coussin Chauffant">
             <h2>Chaleur Douce - Coussin Chauffant</h2>
@@ -148,9 +179,18 @@
             <?php if(!isset($_SESSION['id'])){ ?>
                 <a href="login.php"><button>Ajouter au panier</button></a>
             <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
+                <form method="post" action="">
+                    <input type="hidden" name="product_id" value="13">
+                    <input type="hidden" name="product_name" value="Chaleur Douce - Coussin Chauffant">
+                    <input type="hidden" name="product_price" value="24.99">
+                    <input type="hidden" name="product_quantity" value="1">
+                    <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                </form>
             <?php } ?>
         </div>
+
+
+
         <div class="product-card">
             <img src="img/The-Bien-etre-Serenite.jpg" alt="Thé Bien-être et Sérénité">
             <h2>Thé Bien-être et Sérénité</h2>
@@ -158,9 +198,18 @@
             <?php if(!isset($_SESSION['id'])){ ?>
                 <a href="login.php"><button>Ajouter au panier</button></a>
             <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
+                <form method="post" action="">
+                    <input type="hidden" name="product_id" value="14">
+                    <input type="hidden" name="product_name" value="Thé Bien-être et Sérénité">
+                    <input type="hidden" name="product_price" value="8.99">
+                    <input type="hidden" name="product_quantity" value="1">
+                    <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                </form>
             <?php } ?>
         </div>
+
+
+
         <div class="product-card">
             <img src="img/Bandeau-Relaxant-Yeux.jpg" alt="Bandeau Relaxant pour les Yeux">
             <h2>Bandeau Relaxant pour les Yeux</h2>
@@ -168,9 +217,15 @@
             <?php if(!isset($_SESSION['id'])){ ?>
                 <a href="login.php"><button>Ajouter au panier</button></a>
             <?php } else { ?>
-                <button onclick="addToCart(this)" class="add-to-cart-btn">Ajouter au panier</button>
+                <form method="post" action="">
+                    <input type="hidden" name="product_id" value="15">
+                    <input type="hidden" name="product_name" value="Bandeau Relaxant pour les Yeux">
+                    <input type="hidden" name="product_price" value="14.99">
+                    <input type="hidden" name="product_quantity" value="1">
+                    <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                </form>
             <?php } ?>
-        </div>
+            </div>
         </div>
     </section>
 
